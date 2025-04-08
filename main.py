971a8bf1783e625d5c3cf877cc0abcd4e2553d82
SuelaneSilva <suelanesilva707@gmail.com>
2025-04-08 16:56:13 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,18 +4,14 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(Suelane):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+   print(f'Hi, {Suelane}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+#Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-
-git remote add origin https://github.com/SuelaneSilva/-PL-NYPD-Ferramentas-de-desenvolvimento-em-Python-que-suportam-a-analise-de-dados.git
-git branch -M main
-git push -u origin main
